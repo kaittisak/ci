823b3d247d5d1a92b33185abf52c9880a537527e
kaittisak <mos_kaittisak@hotmail.com>
2016-11-26 14:29:22 +0700

    aa

---
+++ application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 26/11/2559
+ * Time: 13:29
+ */
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        echo "Hello";
+    }
+}
