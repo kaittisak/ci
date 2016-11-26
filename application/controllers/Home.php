355e67acf3aa67f9630be19417bcf0ed5ef32d91
kaittisak <mos_kaittisak@hotmail.com>
2016-11-26 16:04:33 +0700

    เพิ่มbootstrap

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,6 +10,13 @@ class Home extends CI_Controller
 {
     public function index()
     {
-        $this->load->view("home_view");
+        $this->load->view('layout/header_view');
+        $this->load->view('home_view');
+    }
+
+    public function test()
+    {
+       $this->load->view('layout/header_view');
+        $this->load->view('test_view');
     }
 }
---
+++ application/controllers/test_view.php
@@ -0,0 +1,13 @@
+
+
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consequuntur excepturi modi perspiciatis quidem
+    ratione. A, consequatur distinctio ipsum iusto nemo obcaecati quod rerum voluptate voluptates. Animi nostrum
+    sunt vero.
+</div>
+<div>Ad asperiores aspernatur commodi consequatur corporis delectus deleniti eaque eligendi, eos excepturi fugit
+    libero magni, minima non odio omnis optio porro praesentium quaerat reprehenderit sapiente unde veritatis.
+    Cumque, sequi, sit?
+</div>
+<div>Eaque esse impedit iusto laborum maiores natus optio quidem quis vitae voluptas. Ab culpa cumque facilis, fuga
+    fugit hic illo maiores natus nostrum officia perspiciatis quidem quo, quos rerum similique!
+</div>
